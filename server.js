e5d21174c6bccbc51dee21d30971dac24fb40d60
RohakDev <45899405+ImRohak@users.noreply.github.com>
2022-12-08 11:56:00 +0000

    made project and added plugins

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+
+const app = express();
+
+// plugins
+app.use(helmet.noSniff());
+app.use(bodyParser.json())
+
+const port = 3000;
 const path = require('path');
 
 
-app.listen(port, () => {
-  
+app.get('/', (req, res) => {
+  res.send({"status": "active"})
+})
+
+app.listen(port, (res, req) => {
+  console.log(`Running on port ${port}`)
 })
