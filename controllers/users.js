5973abf0164d5852c5b127d99efb21e4ff4f1663
ImRohak <rohak.v@gmail.com>
2022-12-21 17:44:45 +0000

    added user controllers

--- controllers/users.js
+++ controllers/users.js
@@ -5,4 +5,24 @@ exports.db = (req, res) => {
         .then((data) => {
             res.send(data);
         })
+};
+
+exports.addUser = (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const pr = req.body.pr;
+    const todo = req.body.todo;
+    
+    const user = new Users({
+      username: username,
+      password: password,
+      todo: todo,
+      pr: pr
+    })
+
+    user.save()
+      .then((res) => {
+        console.log("Added user to DB")
+      })
+    res.end("Added user");
 }
--- routes/users.js
+++ routes/users.js
@@ -2,33 +2,15 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
-const { db } = require("../controllers/users");
+const { db, addUser } = require("../controllers/users");
 
 router.use((req, res, next) => {
-  console.log(`REQUEST FROM: ${req.headers['x-forwarded-for']}`)
+  console.log(`REQUEST INCOMING: ${req.headers['x-forwarded-for']}`)
   next();
 })
 
 router.get("/db", db);
 
-router.post("/add", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    const pr = req.body.pr;
-    const todo = req.body.todo;
-    
-    const user = new Users({
-      username: username,
-      password: password,
-      todo: todo,
-      pr: pr
-    })
-
-    user.save()
-      .then((res) => {
-        console.log("Added user to DB")
-      })
-    res.end("Added user");
-})
+router.post("/add", addUser);
 
 module.exports = router;
