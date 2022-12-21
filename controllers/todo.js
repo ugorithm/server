fc78da55a85af844ffc618e2e278676e3a4ed8e1
ImRohak <rohak.v@gmail.com>
2022-12-21 17:54:45 +0000

    made todo controllers

---
+++ controllers/todo.js
@@ -0,0 +1,41 @@
+const Users = require("../models/Users");
+
+exports.viewDb = (req, res) => {
+  Users.find()
+    .then((data) => {
+      res.send(data);
+    });
+};
+
+exports.findTodo = (req, res) => {
+  const username = req.body.username;
+
+    Users.find({"username": username})
+     .then((data) => {
+        res.end(`${data[0]["todo"]}`);
+     })
+};
+
+exports.addTodo = (req, res) => {
+  const username = req.body.username; // their username
+  const todo = req.body.todo; // their todo to add
+
+  Users.findOneAndUpdate({"username": username},
+  {
+    $push: { // add their todo to the existing Array of their todo's
+      "todo": todo
+    },
+  }).then(() => res.end("Added todo"))
+    .catch((err) => console.log(`ERROR: ${err}`));
+}
+
+exports.deleteTodo = (req, res) => {
+  const username = req.body.username
+  const todo = req.body.todo;
+
+  Users.findOneAndUpdate({"username": username},{
+    $pull: {
+      "todo": todo
+    },
+  }).then((d) => res.end("Todo deleted"))
+}
--- routes/todo.js
+++ routes/todo.js
@@ -3,50 +3,19 @@ const router = express.Router()
 const mongoose = require("mongoose");
 const Users = require("../models/Users.js")
 
+const { viewDb, findTodo, addTodo, deleteTodo } = require("../controllers/todo");
 
 router.use((req, res, next) => {
   console.log(`REQUEST FROM: ${req.headers['x-forwarded-for']}`)
   next()
 })
 
-router.get("/db", (req, res) => {
-  Users.find()
-    .then((data) => {
-      res.send(data);
-    })
-})
+router.get("/db", viewDb);
 
-router.post("/find", (req, res) => {
-  const username = req.body.username;
+router.post("/find", findTodo);
 
-    Users.find({"username": username})
-     .then((data) => {
-        res.end(`${data[0]["todo"]}`);
-     })
-})
+router.post("/add", addTodo);
 
-router.post("/add", (req, res) => {
-  const username = req.body.username; // their username
-  const todo = req.body.todo; // their todo to add
-
-  Users.findOneAndUpdate({"username": username},
-  {
-    $push: { // add their todo to the existing Array of their todo's
-      "todo": todo
-    },
-  }).then(() => res.end("Added todo"))
-    .catch((err) => console.log(`ERROR: ${err}`));
-});
-
-router.post("/delete", (req, res) => {
-  const username = req.body.username
-  const todo = req.body.todo;
-
-  Users.findOneAndUpdate({"username": username},{
-    $pull: {
-      "todo": todo
-    },
-  }).then((d) => res.end("Todo deleted"))
-})
+router.post("/delete", deleteTodo);
 
 module.exports = router;
